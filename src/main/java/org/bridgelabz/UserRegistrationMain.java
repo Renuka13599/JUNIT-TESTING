02148104de4659d4772eb3768b8e2bcfedc1cb04
renuka13599 <renuka13599@gmail.com>
2021-11-14 20:34:28 +0530

    Added:Program for email test cases is valid or not

--- src/main/java/org/bridgelabz/UserRegistrationMain.java
+++ src/main/java/org/bridgelabz/UserRegistrationMain.java
@@ -1,18 +1,19 @@
 package org.bridgelabz;
 /*Author:Renu
 Date:14/11/21
-Purpose:Program for the test passrule4 validation
+Purpose:Program for the emailtest cases validation
  */
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationMain {
-    public boolean password(String password)
+    public boolean emailIdValidator(String password)
     {
-        String regex="^[0-9a-zA-Z!,@#$&*().]{8,}$";
+        String regex="^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
         Pattern pattern=Pattern.compile(regex);
         Matcher matcher=pattern.matcher("password");
         return matcher.matches();
     }
+
 }
