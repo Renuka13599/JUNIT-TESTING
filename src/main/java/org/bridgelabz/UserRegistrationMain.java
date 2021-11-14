e5688e54f32262a3dea028e775847e9fda5015d9
renuka13599 <renuka13599@gmail.com>
2021-11-14 20:52:37 +0530

    Added:Program for Happy and sad

--- src/main/java/org/bridgelabz/UserRegistrationMain.java
+++ src/main/java/org/bridgelabz/UserRegistrationMain.java
@@ -1,19 +1,21 @@
 package org.bridgelabz;
 /*Author:Renu
 Date:14/11/21
-Purpose:Program for the emailtest cases validation
+Purpose:Program for the happy and sad testcases validation
  */
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationMain {
-    public boolean emailIdValidator(String password)
-    {
-        String regex="^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
-        Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher("password");
-        return matcher.matches();
+    public static String analyseMood(String mood) {
+        if (mood.toLowerCase().contains("happy")) {
+            return "Entry Successful";
+        } else if (mood.toLowerCase().contains("sad")) {
+            return "Entry Failed";
+        } else
+            return null;
     }
+
 
 }
