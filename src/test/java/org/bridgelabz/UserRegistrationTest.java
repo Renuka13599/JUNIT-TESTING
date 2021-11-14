93a53e1e8b98496bf97e35f951057f5321b438e6
renuka13599 <renuka13599@gmail.com>
2021-11-14 20:23:06 +0530

    Added:Program for test  Password validation rule4

--- src/test/java/org/bridgelabz/UserRegistrationTest.java
+++ src/test/java/org/bridgelabz/UserRegistrationTest.java
@@ -8,14 +8,14 @@ import org.junit.Test;
 public class UserRegistrationTest {
     UserRegistrationMain userRegistration=new UserRegistrationMain();
     @Test
-    public void givenPasswordRule3_WhenProper_ShouldReturnTrue() {
+    public void givenPasswordRule4_WhenProper_ShouldReturnTrue() {
         boolean result = userRegistration.password(reuuka@123);
         Assert.assertEquals(true, result);
     }
 
     @Test
-    public void givenPasswordRule3_WhenNotProper_ShouldReturnFlase() {
-        boolean result = userRegistration.password(ren@ta);
+    public void givenPasswordRule4_WhenNotProper_ShouldReturnFlase() {
+        boolean result = userRegistration.password(ren45);
         Assert.assertEquals(false, result);
     }
 }
