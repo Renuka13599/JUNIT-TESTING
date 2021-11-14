0246499b06f0eabd8a30d1fc87bb0b31ad89a2fc
Renuka13599 <91589849+Renuka13599@users.noreply.github.com>
2021-11-14 20:25:52 +0530

    Update UserRegistrationTest.java

--- src/test/java/org/bridgelabz/UserRegistrationTest.java
+++ src/test/java/org/bridgelabz/UserRegistrationTest.java
@@ -9,13 +9,13 @@ public class UserRegistrationTest {
     UserRegistrationMain userRegistration=new UserRegistrationMain();
     @Test
     public void givenPassword_WhenProper_ShouldReturnTrue() {
-        boolean result = userRegistration.password(reNuka@123);
+        boolean result = userRegistration.password(reNuka);
         Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenPassword_WhenNotProper_ShouldReturnFlase() {
-        boolean result = userRegistration.password(ren@13);
+        boolean result = userRegistration.password(ren13);
         Assert.assertEquals(false, result);
     }
 }
